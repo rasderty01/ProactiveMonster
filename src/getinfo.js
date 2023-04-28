6fa5c9e23e170138e8271137dbc5ce0748361f8f
rasderty01 <128894207+rasderty01@users.noreply.github.com>
2023-04-29 00:38:26 +0800

    test

--- src/getinfo.js
+++ src/getinfo.js
@@ -163,8 +163,8 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "../public")));
-app.use(express.static(path.join(__dirname, "../dist")));
+app.use("/public", express.static(path.join(__dirname, "../public")));
+app.use("/dist", express.static(path.join(__dirname, "../dist")));
 // Serve static files
 
 app.post("/getinfo", async (req, res) => {
